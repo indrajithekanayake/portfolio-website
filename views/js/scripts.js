bbc4a4996e4dfc600e35ec8448e73df3d8d1d98e
Indrajith Ekanayake <ekanayakeindrajith@gmail.com>
2023-11-18 15:55:17 +0530

    add code

---
+++ views/js/scripts.js
@@ -0,0 +1,13 @@
+function toggleReadMore(btn) {
+    var moreText = btn.previousElementSibling;
+    var btnText = btn;
+  
+    if (moreText.style.display === "inline") {
+      btnText.innerHTML = "Read More";
+      moreText.style.display = "none";
+    } else {
+      btnText.innerHTML = "Read Less";
+      moreText.style.display = "inline";
+    }
+  }
+  
