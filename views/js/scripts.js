06d17ffa2e123903c9576f697b180db72a09a0d1
Indrajith Ekanayake <ekanayakeindrajith@gmail.com>
2023-11-18 19:13:59 +0530

    add

--- views/js/scripts.js
+++ views/js/scripts.js
@@ -1,13 +1,19 @@
 function toggleReadMore(btn) {
-    var moreText = btn.previousElementSibling;
-    var btnText = btn;
-  
-    if (moreText.style.display === "inline") {
-      btnText.innerHTML = "Read More";
-      moreText.style.display = "none";
-    } else {
-      btnText.innerHTML = "Read Less";
-      moreText.style.display = "inline";
-    }
+  var moreText = btn.previousElementSibling;
+  var btnText = btn;
+
+  if (moreText.style.display === "inline") {
+    btnText.innerHTML = "Read More";
+    moreText.style.display = "none";
+  } else {
+    btnText.innerHTML = "Read Less";
+    moreText.style.display = "inline";
   }
-  
+}
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
